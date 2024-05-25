329a6e45f602f746259bd79641e9a6c0f35df3c7
NikolasZM <101744161+NikolasZM@users.noreply.github.com>
2024-05-24 20:59:21 -0500

    Update Monedas.cpp

--- Monedas.cpp
+++ Monedas.cpp
@@ -1,13 +1,10 @@
 #include <iostream>
 using namespace std;
 
-int monedas[] = {1 , 2 , 5 , 10 , 20 , 50 , 100 , 200 };
-int respuesta[8];
-
 void cambio(int vuelto, int* mon, int* resp, int n) {
 
 	int act = 0;
-	int j = n-1;
+	int j = n - 1;
 
 	for (int i = 0; i < n; i++) {
 		resp[i] = 0;
@@ -15,16 +12,16 @@ void cambio(int vuelto, int* mon, int* resp, int n) {
 
 	while (act != vuelto) {
 		while (mon[j] > (vuelto - act) && (j > 0)) {
-		j--;
+			j--;
 		}
-	if (j == -1) return;
-	resp[j] = (vuelto - act) / mon[j];
-	act = act + mon[j] * resp[j];
+		if (j == -1) return;
+		resp[j] = (vuelto - act) / mon[j];
+		act = act + mon[j] * resp[j];
 	}
-	
+
 }
 
-void verResp() {
+void verResp(int * respuesta, int * monedas) {
 	cout << "Se dieron:\n";
 	for (int i = 0; i < 8; i++) {
 		cout << respuesta[i] << " moneda(s) de " << monedas[i] << " centavos.\n";
@@ -32,7 +29,9 @@ void verResp() {
 }
 
 int main() {
-	
+	int monedas[] = { 1 , 2 , 5 , 10 , 20 , 50 , 100 , 200 };
+	int respuesta[8];
+
 	cambio(189, monedas, respuesta, 8);
-	verResp();
+	verResp(respuesta, monedas);
 }
